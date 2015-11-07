da6e0c6d0bd78ce7d34e08986931e74d98ab4c6c
arobbins <arobbins@simpleblend.net>
2015-11-07 16:02:14 -0600

    Added GForm shortcode

--- content/themes/foodgroup-child/partials/widget-newsletter.php
+++ content/themes/foodgroup-child/partials/widget-newsletter.php
@@ -1,4 +1,4 @@
 <section class="widget widget-newsletter">
   <h1><?php echo $title; ?></h1>
-  <?php // echo do_shortcode('[wpgform id="695"]'); ?>
+  <?php echo do_shortcode('[wpgform id="695"]'); ?>
 </section>
